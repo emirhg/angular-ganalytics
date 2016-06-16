0eab1dcb1eb731fb4bdd81005689f6288b9993bb
Emir Herrera <emir.herrera@imco.org.mx>
2016-06-16 18:40:58 -0500

    Tracking code setup added

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # angular-ganalytics
-Google Analytics for AngularJS. Tracks every single view instead of just the main index.html
+Google Analytics for AngularJS. Tracks every single view instead of just the main index.
 
+##Installation
+
+    bower install --save angular-gnalytics
+
+##Usage
+
+Add the module `analytics` to your app
+
+In the app configuration set the tracking code
+
+    trackingCodeProvider.setTrackingCode('UA-XXXXXXX-X');
--- angular-ganalytics.js
+++ angular-ganalytics.js
@@ -3,11 +3,35 @@
  * @name angular-ganalytics
  * @description Tracking of individual pages.
  */
-(function(){
-  'use strict';
-  angular.module('analytics',[]).run(function($rootScope,$window,$location) {
-    $rootScope.$on('$viewContentLoaded', function() {
-      $window.ga('send', 'pageview', $location.path());
-    });
-  });
-})();
+'use strict';
+angular.module('analytics',[])
+	.provider('trackingCode', function googleAnalyticsProvider(){
+		var trackingCode;
+		this.setTrackingCode = function (ua){
+			trackingCode = ua;
+		};
+
+		this.$get = function(){
+			return trackingCode;
+		}
+	})
+	.run(function($rootScope,$window,$location, trackingCode) {
+		(function(i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function() {
+			    (i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+			    m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+		ga('create', trackingCode);
+
+
+		$rootScope.$on('$viewContentLoaded', function() {
+			$window.ga('send', 'pageview', $location.path());
+		});
+	});
